022ad7901a10b0a12f1d60c5aadd0b5bb398ebef
Jonathan Gagne <jonathan@pcgamesupply.com>
2016-09-17 09:28:58 -0600

    Class 001

--- Class/Formation 001/Class/Class/Bonjour.cs
+++ Class/Formation 001/Class/Class/Bonjour.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +7,11 @@ using System.Windows.Forms;
 
 namespace Class
 {
-    public partial class Form1 : Form
+    class Bonjour
     {
-        public Form1()
+        public Bonjour()
         {
-            InitializeComponent();
-            new Bonjour();
+            MessageBox.Show("New Object!");
         }
     }
 }
