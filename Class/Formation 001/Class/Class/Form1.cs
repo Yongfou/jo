da6813f131a9af6878ec335a98b771ea33ae947a
Jonathan Gagne <jonathan@pcgamesupply.com>
2016-09-17 08:58:35 -0600

    Starting Class

--- Class/Formation 001/Class/Class/Form1.cs
+++ Class/Formation 001/Class/Class/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Class_001
+namespace Class
 {
     public partial class Form1 : Form
     {
--- Class/Formation 001/Class/Class/Properties/Settings.Designer.cs
+++ Class/Formation 001/Class/Class/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Class_001.Properties
+namespace Class.Properties
 {
 
 
